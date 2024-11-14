34ed1a50c1d7601821ca015fa3e3d4a78e4ce8b5
Vitormarcolin2023 <marcolinvh@gmail.com>
2024-11-14 20:08:25 -0300

    aaaaaaaaaaaaa

---
+++ JS/javascriptIndex.js
@@ -0,0 +1,6 @@
+let buttonAcessar = document.getElementById("acessar");
+
+    buttonAcessar.addEventListener("click", (e)=>{
+       e.preventDefault();
+       console.log("Botão Clicado");
+    })
